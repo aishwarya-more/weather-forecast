e35788ed8e6611a663d46a32eea88658ab16d7e2
aishwaryamore <aishwaryamore>
2018-03-16 21:30:34 +0530

    Value from text file is shown in the form

---
+++ WeatherPrediction/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WeatherPrediction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(75, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weather Forecast";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(135, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Current Value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightGreen;
+            this.label3.Location = new System.Drawing.Point(181, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 59);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(434, 360);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Weather  Forecast";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WeatherPrediction/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherPrediction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                FtpWebRequest ftpReq = (FtpWebRequest)WebRequest.Create("ftp://aishwarya-more.square7.ch/a.txt");
+
+                ftpReq.UseBinary = true;
+                ftpReq.Method = WebRequestMethods.Ftp.UploadFile;
+                ftpReq.Credentials = new NetworkCredential("aishwarya-more", "diadulce");
+
+                byte[] b = File.ReadAllBytes(@"C:\input.txt");
+                string value = File.ReadAllText(@"C:\input.txt", System.Text.Encoding.Default);
+                ftpReq.ContentLength = b.Length;
+                using (Stream s = ftpReq.GetRequestStream())
+                {
+                    s.Write(b, 0, b.Length);
+                }
+
+                FtpWebResponse ftpResp = (FtpWebResponse)ftpReq.GetResponse();
+
+                if (ftpResp != null)
+                {
+                    if (ftpResp.StatusDescription.StartsWith("226"))
+                    {
+                        label3.Text = value;
+                    }
+                    else
+                    {
+                        label3.Text = "File Not Uploaded";
+
+
+                    }
+                }
+            }
+
+            catch (Exception se)
+            {
+                MessageBox.Show(se.ToString(), "Exception");
+            }
+
+        }
+    }
+}
